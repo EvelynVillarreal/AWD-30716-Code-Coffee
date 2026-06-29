041f315646e4631a890dda4405154963468dba8a
EvelynVillarreal <ehvillarreal@espe.edu.ec>
2026-06-28 22:11:04 -0500

    Fix AWS EB start.sh to use python venv

--- 06Code/crud-service/start.sh
+++ 06Code/crud-service/start.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-# Install Python dependencies
-pip3 install -r requirements.txt
+# Install Python dependencies in a local virtual environment (required on AWS Linux)
+python3 -m venv venv
+./venv/bin/pip install -r requirements.txt
 
-# Start the Python FastAPI server in the background
-python3 -m uvicorn src.modules.product.main:app --port 8000 &
+# Start the Python FastAPI server in the background using the venv
+./venv/bin/python -m uvicorn src.modules.product.main:app --port 8000 &
 
 # Start the Node.js Express server in the foreground
 node dist/app.js
